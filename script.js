fe6b8324ad436931a676728d0d42fd22aa39bd9c
BiswajitAich <121601439+BiswajitAich@users.noreply.github.com>
2023-04-07 01:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ function search() {
     let input = document.getElementById('search-box').value;
     input = input.toLowerCase();
     let products = document.getElementsByClassName('card');
+    let noResults = document.getElementById('no-results');
 
+    let hasResults = false;
     for (let i = 0; i < products.length; i++) {
         if (!products[i].innerHTML.toLowerCase().includes(input)) {
             products[i].style.display="none";
         }
         else {
             products[i].style.display="block";
+            hasResults = true;
         }
+    }
+
+    if (!hasResults) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
     }
 }
