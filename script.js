652d5a224e6784ccaea62ee6718da8743fc2c416
BiswajitAich <121601439+BiswajitAich@users.noreply.github.com>
2023-05-04 22:11:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,71 @@ function search() {
     let products = document.getElementsByClassName('card');
     let noResults = document.getElementById('no-results');
 
+    let slider = document.querySelector(".slider");
+
     let hasResults = false;
     for (let i = 0; i < products.length; i++) {
         if (!products[i].innerHTML.toLowerCase().includes(input)) {
-            products[i].style.display="none";
+            products[i].style.display = "none";
         }
         else {
-            products[i].style.display="block";
+            products[i].style.display = "block";
             hasResults = true;
         }
     }
 
     if (!hasResults) {
         noResults.style.display = "block";
+        slider.style.display = "none";
     } else {
         noResults.style.display = "none";
+        slider.style.display = "block";
     }
 }
+
+
+
+
+
+
+// Get references to the buttons and product container
+const preBtn = document.querySelector('.pre-btn');
+const nxtBtn = document.querySelector('.nxt-btn');
+const productContainer = document.querySelector('.product-container_slider');
+
+// Define a variable to keep track of the current product index
+let currentProductIndex = 0;
+
+// Add click event listeners to the buttons
+preBtn.addEventListener('click', () => {
+  // Decrement the current product index
+  currentProductIndex--;
+
+  // If the index goes below 0, wrap around to the end
+  if (currentProductIndex < 0) {
+    currentProductIndex = productContainer.children.length - 1;
+  }
+
+  // Scroll the product container to the left to show the previous product
+  productContainer.scrollTo({
+    left: productContainer.children[currentProductIndex].offsetLeft,
+    behavior: 'smooth'
+  });
+});
+
+nxtBtn.addEventListener('click', () => {
+  // Increment the current product index
+  currentProductIndex++;
+
+  // If the index goes past the end, wrap around to the beginning
+  if (currentProductIndex >= productContainer.children.length) {
+    currentProductIndex = 0;
+  }
+
+  // Scroll the product container to the left to show the next product
+  productContainer.scrollTo({
+    left: productContainer.children[currentProductIndex].offsetLeft,
+    behavior: 'smooth'
+  });
+});
+
