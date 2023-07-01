528751d7deadbd0c07e9c129b9a99e8f90b003c9
BiswajitAich <121601439+BiswajitAich@users.noreply.github.com>
2023-07-02 00:14:27 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open('lilastore').then(cache => {
+      return cache.addAll([
+        'https://biswajitaich.github.io/lilastore/',
+        'index.html',
+        'style.css',
+        'utils.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event:', event);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('beforeinstallprompt', event => {
+  event.preventDefault();
+});
