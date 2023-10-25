6080c8fcb842212b00fa239fc758dbe4085931da
BISWAJIT AICH <0000biswajitaich@gmail.com>
2023-10-26 03:25:17 +0530

    FirstDeployDended

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   // scope: '/app',
-  sw: '/sw.js',
+  sw: '/lilastore/public/sw.js',
 
 })
 
