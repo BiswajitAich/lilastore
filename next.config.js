12f06eb3d7f729a6d6c41ba83ffa51513dfd44a3
BISWAJIT AICH <121601439+BiswajitAich@users.noreply.github.com>
2024-02-16 19:29:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   // scope: '/app',
-  sw: '/lilastore/public/sw.js',
+  sw: '/public/sw.js',
 })
 
 module.exports = withPWA({
