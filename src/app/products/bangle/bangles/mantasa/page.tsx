a615d5332caba5acb194862628a893e5001d32af
BISWAJIT AICH <0000biswajitaich@gmail.com>
2024-02-24 21:40:56 +0530

    seo tittle fix & golden necklace/fancy data change[24-02-2024/21:40]

--- src/app/products/bangle/bangles/mantasa/page.tsx
+++ src/app/products/bangle/bangles/mantasa/page.tsx
@@ -1,57 +1,12 @@
-"use client"
-import React from 'react'
-import productData from '../../../../../../public/data/bangle/mantasa.json'
-import { CldImage } from 'next-cloudinary'
-import StyleScript from '../../../../styles/products.module.css'
-import Link from 'next/link'
-import UseReveal from '@/app/components/effects/UseReveal'
+import MantasaClient from "./MantasaClient"
 
-const Mantasa: React.FC = () => {
-    const refs: React.RefObject<HTMLAnchorElement>[] = productData.map(() => UseReveal());
-
-    return (
-        <div className={StyleScript.body} >
-            <div className={StyleScript.productBody}>
-                <h3>Mantasa Collection<div /></h3>
-                <div className={StyleScript.productContainer}>
-                    {productData.map((material, idx) => (
-                        <Link href={`/products/bangle/bangles/mantasa/${material.id}`} ref={refs[idx]} className={StyleScript.reveal} key={idx}>
-                            <div className={StyleScript.productCard} >
-                                <div className={StyleScript.imageDiv}  >
-                                    {
-                                        <CldImage
-                                            itemType='img'
-                                            src={material.url}
-                                            alt={`Mantasa ${idx + 1}`}
-                                            width={400}
-                                            height={600}
-                                            loading='lazy'
-                                            style={{
-                                                borderRadius: "12px",
-                                            }}
-                                            onLoad={(e) => {
-                                                const target = e.target as HTMLImageElement;
-                                                    target.classList.remove(StyleScript.animation);
-                                                    target.style.opacity = "1";
-                                            }}
-                                            className={StyleScript.animation}
-                                        />
-                                    }
-                                </div>
-                                <div className={StyleScript.design}/>
-                                <div className={StyleScript.details}>
-                                    <p style={{
-                                        fontWeight: "bold",
-                                        color: "green"
-                                    }}>Rs {material.price}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+const Mantasa = () => {
+    return (<MantasaClient />)
 }
-
 export default Mantasa
+
+export function generateMetadata() {
+    return {
+        title: "Mantasa Collection"
+    }
+}
