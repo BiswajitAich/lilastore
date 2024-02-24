f9a01d9487bbf4e1532e640f51d18f8f9c5d7465
BISWAJIT AICH <0000biswajitaich@gmail.com>
2024-02-24 21:52:31 +0530

    seo tittle stone earring fix[24-02-2024/21:52]

--- src/app/products/earring/earrings/adstone-earring/page.tsx
+++ src/app/products/earring/earrings/adstone-earring/page.tsx
@@ -1,57 +1,12 @@
-"use client"
-import React from 'react'
-import productData from '../../../../../../public/data/earring/adstone-earring.json'
-import { CldImage } from 'next-cloudinary'
-import StyleScript from '../../../../styles/products.module.css'
-import Link from 'next/link'
-import UseReveal from '@/app/components/effects/UseReveal'
+import StoneEarringClient from "./StoneEarringClient"
 
-const ADStoneEarring: React.FC = () => {
-    const refs: React.RefObject<HTMLAnchorElement>[] = productData.map(() => UseReveal());
-
-    return (
-        <div className={StyleScript.body} >
-            <div className={StyleScript.productBody}>
-                <h3>Stone Earring Collection<div /></h3>
-                <div className={StyleScript.productContainer}>
-                    {productData.map((material, idx) => (
-                        <Link href={`/products/earring/earrings/adstone-earring/${material.id}`} ref={refs[idx]} className={StyleScript.reveal} key={idx}>
-                            <div className={StyleScript.productCard} >
-                                <div className={StyleScript.imageDiv}  >
-                                    {
-                                        <CldImage
-                                            itemType='img'
-                                            src={material.url}
-                                            alt={`Stone Earring ${idx + 1}`}
-                                            width={400}
-                                            height={600}
-                                            loading='lazy'
-                                            style={{
-                                                borderRadius: "12px",
-                                            }}
-                                            onLoad={(e) => {
-                                                const target = e.target as HTMLImageElement;
-                                                    target.classList.remove(StyleScript.animation);
-                                                    target.style.opacity = "1";
-                                            }}
-                                            className={StyleScript.animation}
-                                        />
-                                    }
-                                </div>
-                                <div className={StyleScript.design}/>
-                                <div className={StyleScript.details}>
-                                    <p style={{
-                                        fontWeight: "bold",
-                                        color: "green"
-                                    }}>Rs {material.price}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+const StoneEarring = () => {
+    return (<StoneEarringClient />)
 }
+export default StoneEarring
 
-export default ADStoneEarring
+export function generateMetadata() {
+    return {
+        title: "Stone Earring Collection"
+    }
+}
