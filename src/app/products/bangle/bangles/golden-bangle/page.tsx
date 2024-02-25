c06896114328d1b051e91fc1d7029b7c981bcb9e
Biswajit Aich <0000biswajitaich@gmail.com.com>
2024-02-25 23:15:35 +0530

    api added for db fixed some build error[24-02-2024/23:15]

--- src/app/products/bangle/bangles/golden-bangle/page.tsx
+++ src/app/products/bangle/bangles/golden-bangle/page.tsx
@@ -2,38 +2,39 @@ import GlodenBangleClient from "./GoldenBangleClient/page";
 
 let productData: any | null = null;
 
-export async function fetchData() {
-    
-    try {
-        const base = process.env.NEXT_PUBLIC_BASE_URL || window.location.origin;
-        const resp = await fetch(`${base}/api/fetchData`, {
-            method: "POST",
-            body: JSON.stringify({
-                searchName: "bangle/golden-bangle"
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            next: {
-                revalidate: 10000,
-            },
-        })
-        if (resp.ok) {
-            productData = await resp.json();
-            // console.log("productData....",productData)
-        } else {
-            productData = null;
-        }
-
-
-    } catch (error) {
-        console.log("errrrrrrrrrrrrr", error)
-    }
-
-}
 
 
 const GoldenBangle = async () => {
+    async function fetchData() {
+    
+        try {
+            const base = process.env.NEXT_PUBLIC_BASE_URL || window.location.origin;
+            const resp = await fetch(`${base}/api/fetchData`, {
+                method: "POST",
+                body: JSON.stringify({
+                    searchName: "bangle/golden-bangle"
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                next: {
+                    revalidate: 10000,
+                },
+            })
+            if (resp.ok) {
+                productData = await resp.json();
+                // console.log("productData....",productData)
+            } else {
+                productData = null;
+            }
+    
+    
+        } catch (error) {
+            console.log("errrrrrrrrrrrrr", error)
+        }
+    
+    }
+    
     if(!productData) await fetchData()
 
     return (<GlodenBangleClient  ProductData={productData}/>)
--- src/app/products/earring/earrings/fancy-earring/page.tsx
+++ src/app/products/earring/earrings/fancy-earring/page.tsx
@@ -2,37 +2,38 @@ import FancyEarringClient from "./FancyEarringClient/page";
 
 let productData: any | null = null;
 
-export async function fetchData() {
-    
-    try {
-        const base = process.env.NEXT_PUBLIC_BASE_URL || window.location.origin;
-        const resp = await fetch(`${base}/api/fetchData`, {
-            method: "POST",
-            body: JSON.stringify({
-                searchName: "earring/fancy-earring"
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            next: {
-                revalidate: 10000,
-            },
-        })
-        if (resp.ok) {
-            productData = await resp.json();
-            // console.log("productData....",productData)
-        } else {
-            productData = null;
-        }
-
-
-    } catch (error) {
-        console.log("errrrrrrrrrrrrr", error)
-    }
-
-}
 
 const StoneEarring = async () => {
+    async function fetchData() {
+    
+        try {
+            const base = process.env.NEXT_PUBLIC_BASE_URL || window.location.origin;
+            const resp = await fetch(`${base}/api/fetchData`, {
+                method: "POST",
+                body: JSON.stringify({
+                    searchName: "earring/fancy-earring"
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                next: {
+                    revalidate: 10000,
+                },
+            })
+            if (resp.ok) {
+                productData = await resp.json();
+                // console.log("productData....",productData)
+            } else {
+                productData = null;
+            }
+    
+    
+        } catch (error) {
+            console.log("errrrrrrrrrrrrr", error)
+        }
+    
+    }
+    
     if(!productData) await fetchData()
 
     return (<FancyEarringClient ProductData={productData} />)
