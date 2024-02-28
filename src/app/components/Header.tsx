084dfa63d2b3b6d166cdb77cdf7c658ac675604e
Biswajit Aich <0000biswajitaich@gmail.com.com>
2024-02-28 22:39:34 +0530

    search bar fix[28-02-2024/22:39]

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,15 @@
 "use client"
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import headerStyles from './../styles/header.module.css'
-import productData from './../../../public/data/search/searchData.json'
+import SearchBar from './dataDisplayComponents/SearchBar';
 
 
-interface Product {
-  name: string,
-  link: string
-}
+
 
 
 const Header = () => {
   const [logo, setLogo] = useState(false);
   // const [show, setShow] = useState(true);
-  const [searchQuery, setSearchQuery] = useState<string>('');
-  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-  const inputRef = useRef<HTMLInputElement | null>(null);
 
 
   useEffect(() => {
@@ -37,27 +31,6 @@ const Header = () => {
   // }
   // }
 
-  const handleSearch = (e: { target: { value: any } }) => {
-    const inputValue = e.target.value;
-    setSearchQuery(inputValue);
-
-    const filteredResults = productData.filter((product) => {
-      return product.name && product.name.toLowerCase().includes(inputValue.toLowerCase());
-    });
-
-    setFilteredProducts(filteredResults.slice(0, 5));
-  }
-
-  const handleSearchBtn = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }
-  const handleSearchClear = () => {
-    if (inputRef.current) {
-      setSearchQuery("");
-    }
-  }
 
 
 
@@ -68,7 +41,7 @@ const Header = () => {
       {logo ? (
         <img
           src={"./images/logos/ls-logo.webp"}
-          style={{ minWidth: "50px", maxWidth: "50px", width: "50px", height: "50px", boxShadow: "8px 0px 8px #001341", backgroundColor: "#001341"}}
+          style={{ minWidth: "50px", maxWidth: "50px", width: "50px", height: "50px", boxShadow: "8px 0px 8px #001341", backgroundColor: "#001341" }}
           alt='logo'
           placeholder='blur'
         />
@@ -84,45 +57,10 @@ const Header = () => {
       {/* logo */}
 
       {/* search */}
-      <div className={headerStyles.headerDiv}>
-        <input
-          type='text'
-          placeholder='Search for products...'
-          id={headerStyles.searchInput}
-          value={searchQuery}
-          onChange={handleSearch}
-          ref={inputRef}
-        />
-        {searchQuery === "" ? (
-          <button className={headerStyles.search} aria-label="Search" onClick={handleSearchBtn}>
-            <div className={headerStyles.circle}></div>
-            <div className={headerStyles.handle}></div>
-          </button>
-        ) : (
-          <button className={headerStyles.search} aria-label="Search" onClick={handleSearchClear}>
-            <div className={headerStyles.crossbar1} style={{ width: "25px" }}></div>
-            <div className={headerStyles.crossbar2} style={{ width: "25px" }}></div>
-          </button>
-        )
-        }
-      </div>
+      <SearchBar />
       {/* search */}
 
-      <div className={headerStyles.searchResult}>
-        {searchQuery !== "" ? (
-          filteredProducts.length === 0 ? (
-            <div>No product found!</div>
-          ) : (
-            filteredProducts.map((product: Product, id) => (
-              product.name && product.link &&
-              <div key={id}>
-                <a href={product.link}>{product.name}</a>
-              </div>
 
-            ))
-          )
-        ) : null}
-      </div>
 
 
 
---
+++ src/app/components/dataDisplayComponents/SearchBar.tsx
@@ -0,0 +1,85 @@
+import { Key, useRef, useState } from 'react';
+import productData from '../../../../public/data/search/searchData.json'
+import headerStyles from '../../styles/header.module.css'
+interface Product {
+    name: string,
+    link: string
+}
+
+const SearchBar: React.FC = () => {
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+
+    const handleSearch = (e: { target: { value: any } }) => {
+        const inputValue = e.target.value;
+        setSearchQuery(inputValue);
+
+        const filteredResults = productData.filter((product) => {
+            return product.name && product.name.toLowerCase().includes(inputValue.toLowerCase());
+        });
+
+        setFilteredProducts(filteredResults.slice(0, 5));
+    }
+
+    const handleSearchBtn = () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
+    const handleSearchClear = () => {
+        if (inputRef.current) {
+            setSearchQuery("");
+        }
+    }
+
+    return (
+        <>
+            <div className={headerStyles.headerDiv}>
+                <input
+                    type='text'
+                    placeholder='Search for products...'
+                    id={headerStyles.searchInput}
+                    value={searchQuery}
+                    onChange={handleSearch}
+                    ref={inputRef}
+                />
+                {searchQuery === "" ? (
+                    <button className={headerStyles.search} aria-label="Search" onClick={handleSearchBtn}>
+                        <div className={headerStyles.circle}></div>
+                        <div className={headerStyles.handle}></div>
+                    </button>
+                ) : (
+                    <button className={headerStyles.search} aria-label="Search" onClick={handleSearchClear}>
+                        <div className={headerStyles.crossbar1} style={{ width: "25px" }}></div>
+                        <div className={headerStyles.crossbar2} style={{ width: "25px" }}></div>
+                    </button>
+                )
+                }
+            </div>
+            <div className={headerStyles.searchResult}>
+                {searchQuery !== "" ? (
+                    <div className={headerStyles.fullDiv}>
+                        {searchQuery !== "" ? (
+
+                            filteredProducts.length === 0 ? (
+                                <div style={{ maxWidth: "50vw" }}>No product found!</div>
+                            ) : (
+                                filteredProducts.map((product: Product, id: Key | null | undefined) => (
+                                    product.name && product.link &&
+                                    <a href={product.link} key={id}>
+                                        <div >
+                                            {product.name}
+                                        </div>
+                                    </a>
+                                ))
+                            )
+                        ) : null}
+                    </div>
+                ) : null}
+            </div>
+        </>
+    )
+}
+export default SearchBar
