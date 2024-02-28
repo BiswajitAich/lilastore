106b9af2ae8cc08c5c7e3a27bb39b66a417e0783
Biswajit Aich <0000biswajitaich@gmail.com.com>
2024-02-28 19:02:59 +0530

    error in navigator[28-02-2024/19:02]

--- src/app/products/bangle/BangleTypes/page.tsx
+++ src/app/products/bangle/BangleTypes/page.tsx
@@ -1,17 +1,20 @@
 import BangleTypesClient from "./BangleTypesClient"
 
 const fetchProductsData = async () => {
-    if (typeof navigator !== 'undefined' && !navigator.onLine) return null;
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API}bangle/bangle.json`, {
-        method: "GET",
-        // cache: "force-cache"
-        next: { revalidate: 3600 }
-    })
-    if (res.ok) {
-        const data = await res.json()
-        console.log("static data fetched:", data)
-        return data;
-    } else {
+    if (typeof navigator !== 'undefined' && navigator?.onLine) {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API}bangle/bangle.json`, {
+            method: "GET",
+            // cache: "force-cache"
+            next: { revalidate: 3600}
+        })
+        if (res.ok) {
+            const data = await res.json()
+            console.log("static data fetched:", data)
+            return data;
+        }else{
+            return null
+        }
+    }else{
         return null
     }
 }
