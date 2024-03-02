cc008597fd0b483143bf0a2f2cd087a74fd9d018
BISWAJIT AICH <121601439+BiswajitAich@users.noreply.github.com>
2024-03-02 13:25:35 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useState, useEffect, Suspense } from 'react';
+//"use client"
+import { Suspense } from 'react';
 import styles from './page.module.css'
 import { Header } from './components';
 import Banner from './components/dataDisplayComponents/Banner';
@@ -14,49 +14,49 @@ import Footer from './components/Footer';
 import Cosmetics from './products/cosmetic/Cosmetics';
 import StopContextMenu from './components/simplifiedComponents/StopContextMenu';
 
-interface BeforeInstallPromptEvent extends Event {
-  platforms: string[];
-  userChoice: Promise<{
-    outcome: 'accepted' | 'dismissed';
-    platform: string;
-  }>;
-  prompt(): Promise<void>;
-}
+//interface BeforeInstallPromptEvent extends Event {
+//  platforms: string[];
+//  userChoice: Promise<{
+  //  outcome: 'accepted' | 'dismissed';
+  //  platform: string;
+ // }>;
+ // prompt(): Promise<void>;
+//}
 
 
 export default function Home() {
 
-  const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (event: Event) => {
-      if ('prompt' in event) {
-        event.preventDefault();
-        setDeferredPrompt(event as BeforeInstallPromptEvent);
-      }
-    };
-    const handleAutoPrompt = () => {
-      if (deferredPrompt) {
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult: { outcome: string; }) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          setDeferredPrompt(null);
-        });
-      }
-    };
+  //const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
+//useEffect(() => {
+  //  const handleBeforeInstallPrompt = (event: Event) => {
+    //  if ('prompt' in event) {
+       // event.preventDefault();
+      //  setDeferredPrompt(event as BeforeInstallPromptEvent);
+    //  }
+   // };
+   // const handleAutoPrompt = () => {
+     // if (deferredPrompt) {
+        //deferredPrompt.prompt();
+       // deferredPrompt.userChoice.then((choiceResult: { outcome: string; }) => {
+         // if (choiceResult.outcome === 'accepted') {
+          //  console.log('User accepted the A2HS prompt');
+         // } else {
+          //  console.log('User dismissed the A2HS prompt');
+        //  }
+      //  setDeferredPrompt(null);
+     //   });
+     // }
+    //};
 
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    const autoPromptTimer = setTimeout(() => {
-      handleAutoPrompt();
-    }, 10000);
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-      clearTimeout(autoPromptTimer);
-    };
-  }, [deferredPrompt]);
+   // window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    //const autoPromptTimer = setTimeout(() => {
+     // handleAutoPrompt();
+   // }, 10000);
+  //  return () => {
+    //  window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    //  clearTimeout(autoPromptTimer);
+    //};
+  //}, [deferredPrompt]);
 
   return (
     <div>
