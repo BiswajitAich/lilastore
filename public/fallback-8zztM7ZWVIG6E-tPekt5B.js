015a6204eaecb7704d87686dd7eaf18b8ec74fc1
Biswajit Aich <0000biswajitaich@gmail.com>
2024-09-22 17:02:19 +0530

    static cache fixed: build

---
+++ public/fallback-8zztM7ZWVIG6E-tPekt5B.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";self.fallback=async e=>{switch(e.destination){case"document":return caches.match("/offline",{ignoreSearch:!0});case"image":return caches.match("/static/some/sad.webp",{ignoreSearch:!0});default:return Response.error()}}})();
